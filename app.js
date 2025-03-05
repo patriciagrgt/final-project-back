b5aa842a3ba0511c27fcea4cd028fc5048eed054
Patricia Gracia Gutierrez <pgg.uxui@gmail.com>
2025-03-05 17:46:59 +0000

    centralized routes

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ import express from "express";
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 import config from "./config";
 config(app);
 
@@ -21,12 +20,11 @@ config(app);
 import indexRoutes from "./routes/index.routes";
 app.use("/api", indexRoutes);
 
-import authRoutes from "./routes/auth.routes";
-app.use("/auth", authRoutes);
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 import errorHandling from "./error-handling";
 errorHandling(app);
+
+import notFoundHandler from "./middlewares/error-handling";
+notFoundHandler(app)
 
 export default app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import userRoutes from "./user.routes.js";
+import authRoutes from "./auth.routes.js";
+import profileRoutes from "./profile.routes.js";
+import projectRoutes from "./project.routes.js";
+
 const router = Router();
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API. Use the available endpoints." });
 });
+
+// Todas las rutas están aquí
+router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
+router.use("/profiles", profileRoutes);
+router.use("/projects", projectRoutes);
 
 export default router;
